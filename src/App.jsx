1aa65b31dc3ced122f4c8593335821d5373f72a7
udit-patel26 <prodeveloper26@gmail.com>
2024-08-28 09:18:35 +0530

    context api added

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import Product from './components/Product.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
+import CartContextProvider from './store/ShoppingCart.jsx';
+
+function App() {
+ 
+
+  return (
+    <>
+    <CartContextProvider>
+      <Header
+        
+      />
+      <Shop  >
+
+      {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+        </Shop>
+        </CartContextProvider>
+    </>
+  );
+}
+
+export default App;
